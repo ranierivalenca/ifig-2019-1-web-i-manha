2d0b3ad43d76ea430c03d64ac9103d78628dae48
Ranieri <ranierivalenca@gmail.com>
2019-03-28 11:31:11 -0300

    Copiando exercício para alterações em aula

---
+++ aula-03-28/index.php
@@ -0,0 +1,53 @@
+<?php
+
+$inputs = [
+    'nome' => 'text',
+    'sobrenome' => 'text',
+    'email' => 'text',
+    'senha' => 'password',
+    'senha2' => 'password'
+];
+// echo "<pre>";
+$usuariosFile = file('users.csv');
+// print_r($usuariosFile);
+// $usuarios = [];
+// print_r($usuarios);
+// echo "\n";
+foreach ($usuariosFile as $usuario) {
+    // print_r($usuario);
+    $usuarioData = explode(',', $usuario);
+    // print_r($usuarioData);
+    $nome = $usuarioData[0];
+    $usuarios[] = $nome; // adiciona o elemento $nome ao final do array $usuarios
+    // print_r($usuarios);
+    // echo "\n";
+}
+
+// echo "</pre>";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Registro de Usuário</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <form action="register.php" method="POST">
+        <?php foreach ($inputs as $name => $type): ?>
+            <input type="<?= $type ?>" name="<?= $name ?>" placeholder="<?= $name ?>">
+        <?php endforeach ?>
+        <input type="submit" value="Enviar">
+    </form>
+    <div class="users">
+        <h1>Livros dos usuários</h1>
+        <ul>
+            <?php foreach ($usuarios as $nome): ?>
+                <li><a href="livros.php?usuario=<?= $nome ?>"><?= $nome ?></a></li>
+            <?php endforeach ?>
+        </ul>
+    </div>
+</body>
+</html>
