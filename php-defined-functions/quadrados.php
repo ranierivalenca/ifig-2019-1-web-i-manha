b1062d0203d662c949b00e86deef63a2866bbc24
Ranieri Valença <ranierivalenca@gmail.com>
2019-03-27 22:23:25 -0300

    Definição inicial de exercício sobre funções

---
+++ php-defined-functions/quadrados.php
@@ -0,0 +1,79 @@
+<?php
+
+function quadrado($n) {
+    return $n * $n;
+}
+
+$numbers = $_POST['numbers'] ?? [];
+if ($numbers != []) {
+    $numbers = explode(',', $numbers);
+}
+
+$squares = [];
+foreach ($numbers as $number) {
+    $squares[$number] = quadrado($number);
+}
+
+$numbers_str = '';
+for($i = 0; $i < sizeof($numbers); $i++) {
+    $numbers_str .= $numbers[$i];
+    if ($i < sizeof($numbers) - 1) {
+        $numbers_str .= ',';
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Quadrado</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        input {
+            display: block;
+            padding: 5px;
+        }
+        h3, span, input, table {
+            margin: 10px;
+        }
+        table, tr, th, td {
+            border: 1px solid #CCC;
+            border-collapse: collapse;
+        }
+        th, td {
+            padding: 5px;
+        }
+        tr:nth-child(2n) {
+            background: #EEE;
+        }
+    </style>
+</head>
+<body>
+    <h3>Quadrados</h3>
+    <form action="" method="POST">
+        <span>Preencha o campo abaixo com uma sequência de números separados por vírgula</span>
+        <input type="text" name="numbers" placeholder="Ex: 1,2,3,5" value="<?= $numbers_str ?>">
+        <input type="submit">
+    </form>
+    <hr>
+    <h3>Quadrados</h3>
+    <table>
+        <tr>
+            <th>Número</th>
+            <th>Quadrado</th>
+        </tr>
+        <?php foreach ($squares as $number => $square): ?>
+            <tr>
+                <td><?= $number ?></td>
+                <td><?= $square ?></td>
+            </tr>
+        <?php endforeach ?>
+    </table>
+</body>
+</html>
