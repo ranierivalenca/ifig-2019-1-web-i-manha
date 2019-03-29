7f768918682d9b4d7898cc369d968ab2443b2460
Ranieri <ranierivalenca@gmail.com>
2019-03-29 11:22:51 -0300

    Melhorando mensagem de autenticação

--- aula-03-28/auth.php
+++ aula-03-28/auth.php
@@ -15,8 +15,20 @@ foreach($usuarios as $usuario) {
 }
 
 ?>
-<?php if (is_logged()): ?>
-    <a href="livros.php">Você está logado</a>
-<?php else: ?>
-    <a href="login.php">Tente novamente</a>
-<?php endif ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php if (is_logged()): ?>
+        <h1>Login ok</h1>
+        <a href="livros.php">Clique para ver os livros</a>
+    <?php else: ?>
+        <h1>E-mail ou senha incorretos</h1>
+        <a href="login.php">Tente novamente</a>
+    <?php endif ?>
+</body>
+</html>
