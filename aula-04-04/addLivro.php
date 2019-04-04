400c0abb5fa724c03e12033702111f0f906b5010
Ranieri <ranierivalenca@gmail.com>
2019-04-04 11:37:10 -0300

    Redirecionamento automático após cadastrar livro

--- aula-04-04/addLivro.php
+++ aula-04-04/addLivro.php
@@ -16,17 +16,6 @@ $data = juntar([$usuarioEmail, $nome, $autor]) . "\n";
 $handle = fopen('livros.csv', 'a');
 fwrite($handle, $data);
 
-?>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <div class="message">
-        <h1>Livro cadastrado</h1>
-        <a href="livros.php">Voltar</a>
-    </div>
-</body>
-</html>
+redirect('livros.php');
 
+?>
