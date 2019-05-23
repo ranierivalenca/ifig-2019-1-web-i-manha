cea4c4f289186a4cf64c28c8b14ed87d3a6db10b
Ranieri <ranierivalenca@gmail.com>
2019-05-23 11:49:15 -0300

    Exemplos de ajax

---
+++ aula-05-23/conteudo.php
@@ -0,0 +1,11 @@
+<?php
+    sleep(3);
+    http_response_code(403);
+?>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab cupiditate repellat nulla dolor libero! Ratione provident nesciunt excepturi numquam non, ad totam sit deserunt odio, expedita asperiores, reprehenderit fugit unde.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta dolore delectus neque deserunt, quo, perferendis quibusdam expedita facere incidunt dolorum vitae illum voluptatibus iste sed recusandae ipsum nesciunt culpa. Nihil.</p>
+<ul>
+    <li><a href="#">hey man</a> </li>
+    <li></li>
+</ul>
